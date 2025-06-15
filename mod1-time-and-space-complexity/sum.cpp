f10be1913f32ffa3a21f22c60f579105be5746e9
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-15 22:55:50 +0600

    data structure

---
+++ mod1-time-and-space-complexity/sum.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int sum;
+
+    // using loop
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     sum+=i;
+    // }
+
+    // using formula
+
+    sum = (n*(n+1))/2;
+
+    cout << sum;
+    
+    return 0;
+}
