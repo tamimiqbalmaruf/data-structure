14981d22f8960836bec09dbe717ac9f681de14ef
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-17 17:57:33 +0600

    Sqrt complexity O(sqrt(N))

---
+++ mod1-time-and-space-complexity/sqrt_complexity_O_sqrtN.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " " << n / i << endl;
+        }
+    }
+
+    return 0;
+}
