ddfecd49095e8759b6fd3894b080ffdec2d0ce02
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-20 11:22:05 +0600

    Vector Capacity functions

---
+++ mod2-STL-vector/Vector_Capacity.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(10);
+
+    // cout << v.size() << endl;
+
+    // v.clear();
+
+    // cout << v.size() << endl;
+    // cout << v[0] << endl;
+
+    v.resize(5, 100);
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
