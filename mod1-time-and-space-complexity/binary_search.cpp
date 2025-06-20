299a5df80c2bed27a0d0d78a8b6a2bedf5c1a07e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-20 10:33:28 +0600

    Vector initialization

--- mod1-time-and-space-complexity/binary_search.cpp
+++ mod1-time-and-space-complexity/binary_search.cpp
@@ -7,6 +7,14 @@ int main()
 
     cin >> n >> q;
 
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+       cin >> a[i];
+    }
+    
+
     
     return 0;
 }
---
+++ mod2-STL-vector/Vector_initialization.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // vector<int>v;
+
+    // vector<int>v(10);
+
+    // vector<int> v(10, 5);
+
+    // vector<int> v2(v);
+
+    // int a[5] = {1,2,3,4,5};
+
+    // vector<int> v(a, a+5);
+
+    vector<int> v ={1,2,3,4,5};
+
+
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
+    // cout << v.size() << endl;
+
+    return 0;
+}
