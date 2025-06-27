7e3e84041ed353347aff68a4c06da50a3bef8b7f
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-06-27 12:35:31 +0600

    Prefix Sum

--- Y_Range_sum_query.cpp
+++ Y_Range_sum_query.cpp
@@ -6,11 +6,20 @@ int main()
     int n, q;
     cin >> n >> q;
 
-    vector<int> v(n);
+    vector<long long int> v(n + 1);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> v[i];
+    }
+
+    vector<long long int> pre(n + 1);
+
+    pre[1] = v[1];
+
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i] = pre[i - 1] + v[i];
     }
 
     while (q--)
@@ -18,10 +27,15 @@ int main()
         int l, r;
         cin >> l >> r;
 
-        int sum = 0;
-        for (int i = l; i <= r; i++)
+        long long int sum = 0;
+
+        if (l == 1)
         {
-            sum += v[i-1];
+            sum = pre[r];
+        }
+        else
+        {
+            sum = pre[r] - pre[l - 1];
         }
 
         cout << sum << endl;
