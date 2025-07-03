c41016d72cd6765f7e2cbd70dfab55d0342044f5
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-07-03 15:31:40 +0600

    Node with Constructor

--- mod5-singly-linked-list/Creating_Node.cpp
+++ mod5-singly-linked-list/Creating_Node.cpp
@@ -3,22 +3,23 @@ using namespace std;
 
 class Node
 {
-    public:
+public:
     int val;
-    Node* next;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    };
 };
 
 int main()
 {
-    Node a,b,c;
-
-    a.val = 10;
-    b.val = 20;
-    c.val = 30;
+    Node a(10), b(20), c(30);
 
     a.next = &b;
     b.next = &c;
-    c.next = NULL;
 
     cout << a.val << endl;
     cout << a.next->val << endl;
