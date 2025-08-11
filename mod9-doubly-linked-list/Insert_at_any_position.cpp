b1412f6ad2cf518190984f86745c6898049775fd
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-12 05:50:38 +0600

    Insert at any position

---
+++ mod9-doubly-linked-list/Insert_at_any_position.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    };
+};
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void insert_at_any_pos(Node *head, int idx, int val)
+{
+    Node *newNode = new Node(val);
+
+    Node *temp = head;
+
+    while (--idx)
+    {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    insert_at_any_pos(head, 2, 100);
+    print_forward(head);
+
+    return 0;
+}
