2af30ffee331dc8ce43e5d09ce476a369e5f3989
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-12 14:26:37 +0600

    List Constructors

---
+++ mod10-STL-list-and-cycle-detection/stl_list.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // list<int> l;
+
+    // list<int> l(10);
+
+    // list<int> l(10, 3);
+    // cout << *l.begin() << endl;
+
+    // for(auto it = l.begin(); it != l.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
+
+    list<int> l = {1,2,3,4,5};
+    // list<int> l2(l); // copy list from l to l2
+
+    // int a[3] = {10, 20, 30};
+    // list<int> l2(a, a+3); // copy array
+
+    vector<int> v = {40, 50, 60};
+    list<int> l2(v.begin(), v.end()); // copy array
+
+
+
+    for(int val : l2){
+        cout << val << endl;
+    }
+
+    // cout << l.size() << endl;
+    return 0;
+}
