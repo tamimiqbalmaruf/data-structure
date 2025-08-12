8fab4d15bc520090b8e17db62a128744d06e3f5c
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-12 15:44:15 +0600

    List Modifiers functions

---
+++ mod10-STL-list-and-cycle-detection/list_modifiers.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l = {10, 20, 30, 40, 50};
+    // list<int> l2;
+
+    // l2 = l;
+    // l2.assign(l.begin(), l.end());
+
+    // l.push_back(100);
+    // l.push_front(100);
+
+    // l.pop_back();
+    // l.pop_front();
+
+    // l.insert(next(l.begin(), 3),35); // insert at any pos
+
+    // list<int> l3 = {100, 200};
+    // l.insert(next(l.begin(), 2), l3.begin(), l3.end()); // insert at any pos multiple value from list
+
+    vector<int> v = {500, 600};
+    l.insert(next(l.begin(), 2), v.begin(), v.end()); // insert at any pos multiple value from vector
+
+    // l.erase(next(l.begin(), 2)); // delete at any pos
+
+    for (int val : l)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
