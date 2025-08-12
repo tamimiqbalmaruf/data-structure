c3ed9089d0fc737460f89c8fc3b062c7d6b253e5
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-12 12:18:57 +0600

    Delete at Tail

---
+++ mod9-doubly-linked-list/Delete_at_tail.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    };
+};
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void delete_at_tail(Node *&head, Node *&tail)
+{
+    if (head == NULL)
+        return;
+
+    Node *deleteNode = tail;
+
+    tail = tail->prev;
+
+    if (tail == NULL)
+    {
+        head = NULL;
+    }
+    else
+    {
+        tail->next = NULL;
+    }
+
+    delete deleteNode;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    delete_at_tail(head, tail);
+    print_forward(head);
+
+    return 0;
+}
