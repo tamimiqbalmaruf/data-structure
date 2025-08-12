ffbf2abc7ac1920cf76a239ca614cac9e9e21fb4
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-12 15:53:33 +0600

    List Modifiers functions II

--- mod10-STL-list-and-cycle-detection/list_modifiers.cpp
+++ mod10-STL-list-and-cycle-detection/list_modifiers.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    list<int> l = {10, 20, 30, 40, 50};
+    list<int> l = {10, 20, 30, 40, 20, 60, 70};
     // list<int> l2;
 
     // l2 = l;
@@ -20,10 +20,22 @@ int main()
     // list<int> l3 = {100, 200};
     // l.insert(next(l.begin(), 2), l3.begin(), l3.end()); // insert at any pos multiple value from list
 
-    vector<int> v = {500, 600};
-    l.insert(next(l.begin(), 2), v.begin(), v.end()); // insert at any pos multiple value from vector
+    // vector<int> v = {500, 600};
+    // l.insert(next(l.begin(), 2), v.begin(), v.end()); // insert at any pos multiple value from vector
 
     // l.erase(next(l.begin(), 2)); // delete at any pos
+
+    // l.erase(next(l.begin(), 2), next(l.begin(), 5)); // delete at any pos multiple value
+
+    // replace(l.begin(), l.end(), 20, 100);
+
+    auto it = find(l.begin(), l.end(), 20);
+
+    if(it == l.end()){
+        cout << "Not found" << endl;
+    }else{
+        cout << "Found" << endl;
+    }
 
     for (int val : l)
     {
