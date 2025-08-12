4386ccd9795ff4e7e1a03f44093ca4166f407eee
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-12 14:34:04 +0600

    List Capacity functions

--- mod10-STL-list-and-cycle-detection/stl_list.cpp
+++ mod10-STL-list-and-cycle-detection/stl_list.cpp
@@ -15,21 +15,29 @@ int main()
     //     cout << *it << endl;
     // }
 
-    list<int> l = {1,2,3,4,5};
+    list<int> l = {1, 2, 3, 4, 5};
     // list<int> l2(l); // copy list from l to l2
 
     // int a[3] = {10, 20, 30};
     // list<int> l2(a, a+3); // copy array
 
-    vector<int> v = {40, 50, 60};
-    list<int> l2(v.begin(), v.end()); // copy array
+    // vector<int> v = {40, 50, 60};
+    // list<int> l2(v.begin(), v.end()); // copy array
 
+    // l.clear(); // clear list
 
+    l.size(); // list size
 
-    for(int val : l2){
+    l.empty() ? cout << "empty" : cout << "not empty" << endl; // empty check
+
+    // l.resize(2); // list resize into 2 size
+    // l.resize(10); // list resize into 10 size
+    l.resize(10, 100); // list resize into 10 size, other val 100
+
+    for (int val : l)
+    {
         cout << val << endl;
     }
 
-    // cout << l.size() << endl;
     return 0;
 }
