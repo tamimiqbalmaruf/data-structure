9d4bb24b9d6626874252540ea77835be3f3ee9cd
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-14 00:13:04 +0600

    Reverse Doubly linked list

---
+++ mod10-STL-list-and-cycle-detection/Reverse_Doubly_linked_list.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    };
+};
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+
+    tail = newNode;
+}
+
+void reverse_doubly_linked_list(Node *head, Node *tail)
+{
+
+    for (Node *i = head, *j = tail; i != j && i->prev != j; i = i->next, j = j->prev)
+    {
+        swap(i->val, j->val);
+    }
+
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+
+    while (cin >> val && val != -1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+
+    reverse_doubly_linked_list(head, tail);
+    print_forward(head);
+
+    return 0;
+}
