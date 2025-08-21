c0771f8356dd6c28300ee380b710d8b7a91c6bc6
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-08-21 11:22:19 +0600

    Queue Input and Output

--- mod14-Queue/Implement_Queue_using_Singly.cpp
+++ mod14-Queue/Implement_Queue_using_Singly.cpp
@@ -70,6 +70,23 @@ public:
 
 int main()
 {
+    myQueue q;
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+
+    while (!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
 
     return 0;
 }
